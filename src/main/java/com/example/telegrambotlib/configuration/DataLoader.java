8d76933f0701724068c884bc8678105c89b2f291
mirkomil_a <mirkomilablayev777@gmail.com>
2023-11-29 18:01:08 +0500

    refactor

--- src/main/java/com/example/telegrambotlib/configuration/DataLoader.java
+++ src/main/java/com/example/telegrambotlib/configuration/DataLoader.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
